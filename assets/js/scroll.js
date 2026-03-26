c08f98cb699a6a341fe0fede929548723ebe04fa
sunnyskyess420 <alively14@gmail.com>
2026-03-25 22:30:28 -0600

    Add files via upload

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,24 +1,40 @@
-// LEGEND HIDE ON SCROLL DOWN
-let lastScrollY = window.scrollY;
+// ALL HEADER ELEMENTS HIDE IN SEQUENCE ON STRAIN LIST SCROLL
+let lastStrainScrollTop = 0;
+let hideTimeout = null;
 
+const strainList = document.querySelector('.strain-list-container');
+const headerImage = document.querySelector('.header-image');
+const searchContainer = document.querySelector('.search-container');
 const legend = document.querySelector('.legend');
-if (legend) {
-  function checkLegendScroll() {
-    let currentScrollY = window.scrollY;
-    const strainList = document.querySelector('.strain-list-container');
+
+const elements = [
+  { el: headerImage, class: 'scroll-hidden', delay: 0 },
+  { el: searchContainer, class: 'scroll-hidden', delay: 100 },
+  { el: legend, class: 'scroll-hidden', delay: 200 }
+].filter(item => item.el);
+
+if (strainList) {
+  function handleStrainScroll() {
+    const scrollingDown = strainList.scrollTop > lastStrainScrollTop && strainList.scrollTop > 50;
     
-    // Add strain list scroll if it exists
-    if (strainList) {
-      currentScrollY += strainList.scrollTop;
-    }
-
-    if (currentScrollY > lastScrollY && currentScrollY > 200) {
-      legend.classList.add('hide');
+    clearTimeout(hideTimeout);
+    
+    if (scrollingDown) {
+      // Hide in perfect sequence
+      elements.forEach(({ el, class: cls, delay }) => {
+        hideTimeout = setTimeout(() => {
+          el.classList.add(cls);
+        }, delay);
+      });
     } else {
-      legend.classList.remove('hide');
+      // Show all immediately when scrolling up
+      elements.forEach(({ el }) => {
+        el.classList.remove('scroll-hidden');
+      });
     }
-
-    lastScrollY = currentScrollY;
+    
+    lastStrainScrollTop = strainList.scrollTop;
   }
-  }
-
+  
+  strainList.addEventListener('scroll', handleStrainScroll, { passive: true });
+}
